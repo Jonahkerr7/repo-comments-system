34b9616167052291aa5e4fa22f8ce6638d2ada40
User <user@example.com>
2026-01-31 22:11:27 -0600

    Initial commit: RepoComments backend and widget
    - Express backend with PostgreSQL
    - GitHub OAuth authentication
    - WebSocket real-time updates
    - Embeddable widget for any website
    - Docker deployment ready

--- backend/dist/websocket.d.ts
+++
@@ -1,8 +0,0 @@
-import { Server as HTTPServer } from 'http';
-import { Server } from 'socket.io';
-export declare function setupWebSocket(httpServer: HTTPServer): Server;
-export declare function broadcastThreadCreated(io: Server, thread: any): void;
-export declare function broadcastThreadUpdated(io: Server, thread: any): void;
-export declare function broadcastMessageAdded(io: Server, message: any, thread: any): void;
-export default setupWebSocket;
-//# sourceMappingURL=websocket.d.ts.map
